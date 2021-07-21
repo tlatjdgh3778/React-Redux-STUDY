3a2c3777ccbcc249471531df97a0c6932b772321
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-21 19:14:54 +0900

    React Redux Tutorials - 21 - useDispatch Hook

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useSelector  } from 'react-redux';
+import { useSelector, useDispatch  } from 'react-redux';
+import { buyCake } from '../redux';
 // useSelector is a hook the react redux library provides which acts as a closed equivalent to the mapStateToProps function 
 
 
 const HooksCakeContainer = () => {
     const numOfCakes = useSelector(state => state.numOfCakes)
-    
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Num of cakes - {numOfCakes}</h2>
-            <button>Buy cake</button>
+            <button onClick={() => dispatch(buyCake())}>Buy cake</button>
         </div>
     )
 }
