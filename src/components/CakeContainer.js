669a21cc90b442dc008ca86ab62d1413b6158dd6
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-21 18:56:23 +0900

    React Redux Tutorials - 18 - connect

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux/index';
 
-const CakeContainer = () => {
+const CakeContainer = (props) => {
     return(
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakes - {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     );
 }
 
-export default CakeContainer;
+// 1. the state from the redux store is mapped to our comp props
+// Define mapStateToProps to access the redux state in your comp
+// It gets the redux state as a parameter.(state is retrieve state properties. ex> state.numOfCakes)
+// Using JSX state.numOfCakes
+const mapStateToProps = state => { // selector
+    return {
+        numOfCakes: state.numOfCakes 
+    }
+}
+// 2. map our dispatch of an asction creator to a prop in our comp
+// To allow us to map action creators to props in our comp
+// Allow us call {props.buyCake}
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+// 3. Connect these two functions with our react comp
+// connect cakeContainer to the redux store
+// basic pattern
+export default connect(
+    mapStateToProps, 
+    mapDispatchToProps
+)(CakeContainer);
---
+++ src/redux/index.js
@@ -0,0 +1,2 @@
+// export all the action creators
+export { buyCake } from './cake/cakeActions';
