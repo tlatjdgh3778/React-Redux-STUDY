476c36fd4edfb75acd5df7776649f74bdc6672b9
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-21 20:50:27 +0900

    React Redux Tutorials - 27 - mapDispatchToProps

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../redux';
 
 const ItemContainer = (props) => {
     return (
         <div>
             <h2>Item - {props.item}</h2>
+            <button onClick={props.buyItem}>Buy Items</button>
         </div>
     )
 }
@@ -22,4 +24,17 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream())
+
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(ItemContainer);
